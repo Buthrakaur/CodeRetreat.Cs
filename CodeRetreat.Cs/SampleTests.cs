e887cd4508197c106b384203aa7480f5c2077274
Filip Kinský <filip@filovo.net>
2014-11-13 22:24:46 +0100

    upgraded solution to xUnit + FluentAssertions

---
+++ CodeRetreat.Cs/SampleTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CodeRetreat.Cs
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void Test()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
